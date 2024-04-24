667e0fb79531bb890e9ef4c7b024192c1ab78284
FreezePunk <gamermomo23@gmail.com>
2024-04-24 21:26:16 +0200

    Niveau2

--- src/bowling/bowling.java
+++ src/bowling/bowling.java
@@ -1,22 +1,29 @@
 package bowling;
 
 public class bowling {
-	private int[] rolls = new int[21];
-	private int currentRoll=0;
-	
-	void roll (int nbQuilles)
-	{
-		rolls[currentRoll++]=nbQuilles;
-	}
-	
-	public int score() {
-		int totalScore = 0;
-		for (int i = 0;i < rolls.length;i++)
-		{
-			totalScore+=rolls[i];
-		}
-		return totalScore;		
-	}
+    private int[] rolls = new int[21];
+    private int currentRoll = 0;
 
-	
+    void roll(int nbQuilles) {
+        rolls[currentRoll++] = nbQuilles;
+    }
+
+    public int score() {
+        int totalScore = 0;
+        int rollIndex = 0;
+        for (int frame = 0; frame < 10; frame++) { // Iterating through 10 frames
+            if (isSpare(rollIndex)) {
+                totalScore += 10 + rolls[rollIndex + 2]; // Add 10 for the spare plus the next roll
+                rollIndex += 2; // Move to the next frame
+            } else {
+                totalScore += rolls[rollIndex] + rolls[rollIndex + 1]; // Add the total pins knocked down in the frame
+                rollIndex += 2; // Move to the next frame
+            }
+        }
+        return totalScore;
+    }
+
+    public boolean isSpare(int index) {
+        return rolls[index] + rolls[index + 1] == 10;
+    }
 }
--- test/bowling/bowlingTest.java
+++ test/bowling/bowlingTest.java
@@ -22,7 +22,9 @@ class bowlingTest {
 	@Test
 	void quilles20_1() {
 		for (int i=0;i<20;i++)
+		{
 			game.roll(1);
+		}	
 		assertEquals(20,game.score());
 	}
 	
@@ -34,5 +36,15 @@ class bowlingTest {
 			game.roll(2);
 		assertEquals(30,game.score());
 	}
+	
+	@Test
+	void Niveau2() {
+		int[] rolls= {7,3,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+		for (int i=0;i<10;i++)
+			game.roll(rolls[i]);
+		assertEquals(18,game.score());
+	}
+	
+	
 	
 }
